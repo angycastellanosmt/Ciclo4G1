e9c0e6ec2ce7b9e1e5ce40833d89bb3943dca184
Angy Castellanos <angycastel@Angys-MacBook-Air.local>
2021-11-25 10:04:08 -0500

    Primer commit

---
+++ src/test/java/co/edu/usa/ciclo4/retouno/RetounoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usa.ciclo4.retouno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetounoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
